cb3bf62d36f3d4b375f4ae1ef466f1546ec1d5de
Nuno Rafael <nunorpj@gmail.com>
2018-03-19 16:56:06 +0000

    added a dialog
    dialog  showing the winning color

--- app/src/main/java/pt/isec/a21240649/connect3/MainActivity.java
+++ app/src/main/java/pt/isec/a21240649/connect3/MainActivity.java
@@ -4,15 +4,22 @@ import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.RotateAnimation;
 import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.math.RoundingMode;
 
@@ -20,23 +27,25 @@ public class MainActivity extends Activity {
 
     //0= yellow, 1 = red
     int activePlayer=0;
-
-    //2 means unplayed
-    int[] gameState= {2,2,2,2,2,2,2,2,2};
-
-    int[][] winningPositions={  {0,1,2},{3,4,5},{6,7,8}, // horizontal
-                                {0,3,6},{1,2,7},{2,5,8}, // vertical
-                                {0,4,8},{2,4,6}};        // diagnal
-
+    public AlertDialog aDHandler;
     boolean gameEnd = false;
     ImageView player;
+    //2 means unplayed
+    int[] gameState= {2,2,2,2,2,2,2,2,2};
+    int[][] winningPositions={  {0,1,2},{3,4,5},{6,7,8}, // horizontal
+                                {0,3,6},{1,4,7},{2,5,8}, // vertical
+                                {0,4,8},{2,4,6}};        // diagnal
+    public LinearLayout llEmp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         player = (ImageView) findViewById(R.id.player);
+
+        llEmp = findViewById(R.id.LLEpate);
+
+        llEmp.setVisibility(View.INVISIBLE);
     }
 
 
@@ -49,7 +58,6 @@ public class MainActivity extends Activity {
         ImageView counter = (ImageView) view;
 
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
-
 
         if (gameState[tappedCounter]==2 && gameEnd==false) {
             counter.setTranslationY(-1000f);
@@ -78,15 +86,11 @@ public class MainActivity extends Activity {
 
 
             AnimationSet animation = new AnimationSet(false);
-
-
             Animation Rollout = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
                     Animation.RELATIVE_TO_SELF, -1.5f, Animation.RELATIVE_TO_SELF,
                    0.0f, Animation.RELATIVE_TO_SELF, 0.0f);
 
-
             Rollout.setDuration(300);
-
 
             Rollout.setAnimationListener(new Animation.AnimationListener() {
                 @Override
@@ -129,18 +133,53 @@ public class MainActivity extends Activity {
             player.startAnimation(animation);
 
 
+            if(gameEnd==true){
+                WIN();
+            }
+        }//end if
+
+
+    }//end func
+
+    public void WIN(){
 
 
 
 
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        View alert =getLayoutInflater().inflate(R.layout.layout,null);
+        ImageView iV=(ImageView) alert.findViewById(R.id.win);
 
 
-
-
-
-
+        if(activePlayer!=0){
+            iV.setImageResource(R.drawable.yellow);
+        }else{
+            iV.setImageResource(R.drawable.red);
         }
 
+        Button btn = alert.findViewById(R.id.BTNalertNewGame);
+
+
+
+        alertDialogBuilder.setView(alert);
+        aDHandler = alertDialogBuilder.show();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                aDHandler.dismiss();
+                reset();
+            }
+        });
+    }
+
+
+    public void reset(){
+        gameEnd=false;
+        for (int i =0; i<9;i++){
+            gameState[i]=2;
+        }
+        setContentView(R.layout.activity_main);
 
     }
 }
